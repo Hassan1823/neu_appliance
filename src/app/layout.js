603898addc2ca330ddea517d72a06480f67f9a56
hassan1823 <hassan.zaib223@gmail.com>
2023-06-05 05:27:45 -0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
+import Header from '@/components/header/Header'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter, Montserrat } from 'next/font/google'
+import Footer from '@/components/footer/Footer'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Montserrat({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+        </body>
     </html>
   )
 }
